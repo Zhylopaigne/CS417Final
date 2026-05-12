0d3734fd71cf2b23fa28d0e442682c3ca104af37
Zhylopaigne <zipthepenguin@gmail.com>
2026-05-11 21:57:45 -0500

    Save/Load System

--- Assets/Scripts/SaveLoad.cs
+++ Assets/Scripts/SaveLoad.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.IO;
 using System.Resources;
 using UnityEngine;
 
@@ -34,24 +36,24 @@ public class Serializable
     public bool routeChosen = false;
 
     [Header("Action Stats")]
-    private int adsBought = 0;
-    private int ralliesHeld = 0;
-    private int prCampaignsRun = 0;
+    public int adsBought = 0;
+    public int ralliesHeld = 0;
+    public int prCampaignsRun = 0;
 
     [Header("Secret Unlocks")]
-    private bool secretEndorsementUnlocked = false;
-    private bool grassrootsMovementUnlocked = false;
-    private bool corporateBackerUnlocked = false;
-    private bool viralMomentUnlocked = false;
+    public bool secretEndorsementUnlocked = false;
+    public bool grassrootsMovementUnlocked = false;
+    public bool corporateBackerUnlocked = false;
+    public bool viralMomentUnlocked = false;
 
     [Header("Timeline")]
     public int currentDay = 1;
 
     public Serializable
     (
-        int[] _campaignResources, int[] _groupSupport, 
-        int[] _actCosts, int[] _roiVals, float[] _bonuses, 
-        string _route, int[] _actions, bool[] _unlocks,
+        List<int> _campaignResources, List<int> _groupSupport, 
+        List<int> _actCosts, List<int> _roiVals, List<float> _bonuses, 
+        string _route, List<int> _actions, List<bool> _unlocks,
         int _curDay
     )
     {
@@ -71,7 +73,7 @@ public class Serializable
         rallyBonus = _bonuses[2];
         adsPenalty = _bonuses[3];
         selectedRoute = _route;
-        routeChosen = selectedRoute != "None";
+        routeChosen = _route != "None";
         adsBought = _actions[0];
         ralliesHeld = _actions[1];
         prCampaignsRun = _actCosts[2];
@@ -96,14 +98,139 @@ public class SaveLoad : MonoBehaviour
     {
         if (autoSave)
         {
-            
+            Load();
         }
 
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
+        if(autoSave){
+            Save();
+        }
+    }
+
+    private string PrepJson()
+    {
+        Serializable serializable = new Serializable
+        (
+            new List<int>{resourceManager.campaignFunds, resourceManager.popularity, resourceManager.credibility}, 
+            new List<int>{resourceManager.youthSupport, resourceManager.workingClassSupport, resourceManager.corporateSupport},
+            new List<int>{resourceManager.adsCost, resourceManager.rallyCost, resourceManager.prCost}, 
+            new List<int>{resourceManager.popularityDollarValue, resourceManager.credibilityDollarValue},
+            new List<float>{resourceManager.fundraisingBonus, resourceManager.prBonus, resourceManager.rallyBonus, resourceManager.adsPenalty}, 
+            resourceManager.selectedRoute, 
+            new List<int>{resourceManager.adsBought, resourceManager.ralliesHeld, resourceManager.prCampaignsRun},
+            new List<bool>{
+                resourceManager.secretEndorsementUnlocked, resourceManager.grassrootsMovementUnlocked, 
+                resourceManager.corporateBackerUnlocked, resourceManager.viralMomentUnlocked
+            },
+            campaignTimelineManager.currentDay
+        );
+
+        return JsonUtility.ToJson(serializable);
+    }
+
+    private void LoadJson(string _jsonString)
+    {
+        Serializable serializable = JsonUtility.FromJson<Serializable>(_jsonString);
+        resourceManager.campaignFunds = serializable.campaignFunds;
+        resourceManager.popularity = serializable.popularity;
+        resourceManager.credibility = serializable.credibility;
+        resourceManager.youthSupport = serializable.youthSupport;
+        resourceManager.workingClassSupport = serializable.workingClassSupport;
+        resourceManager.corporateSupport = serializable.corporateSupport;
+        resourceManager.adsCost = serializable.adsCost;
+        resourceManager.rallyCost = serializable.rallyCost;
+        resourceManager.prCost = serializable.prCost;
+        resourceManager.popularityDollarValue = serializable.popularityDollarValue;
+        resourceManager.credibilityDollarValue = serializable.credibilityDollarValue;
+        resourceManager.fundraisingBonus = serializable.fundraisingBonus;
+        resourceManager.prBonus = serializable.prBonus;
+        resourceManager.rallyBonus = serializable.rallyBonus;
+        resourceManager.adsPenalty = serializable.adsPenalty;
+        resourceManager.selectedRoute = serializable.selectedRoute;
+        campaignMenuManager.routeChosen = serializable.routeChosen;
+        resourceManager.adsBought = serializable.adsBought;
+        resourceManager.ralliesHeld = serializable.ralliesHeld;
+        resourceManager.prCampaignsRun = serializable.prCampaignsRun;
+        resourceManager.secretEndorsementUnlocked = serializable.secretEndorsementUnlocked;
+        resourceManager.grassrootsMovementUnlocked = serializable.grassrootsMovementUnlocked;
+        resourceManager.corporateBackerUnlocked = serializable.corporateBackerUnlocked;
+        resourceManager.viralMomentUnlocked = serializable.viralMomentUnlocked;
+        campaignTimelineManager.currentDay = serializable.currentDay;
+    }
+
+    public void Save()
+    {
+        string path = Path.Combine(Application.dataPath, fileName);
+
+        using (FileStream fs = new FileStream(path, FileMode.Create))
+        {
+            using (StreamWriter writer = new StreamWriter(fs))
+            {
+                writer.Write(PrepJson()); 
+            }
+        }
+    }
+
+    public void Load()
+    {
+        string path = Path.Combine(Application.dataPath, fileName);
+        if (File.Exists(path))
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                using (StreamReader reader = new StreamReader(fs))
+                {
+                    string loadedString = reader.ReadToEnd();
+                    LoadJson(loadedString);
+                }
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        string path = Path.Combine(Application.dataPath, fileName);
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+        resourceManager.campaignFunds = 2000;
+        resourceManager.popularity = 50;
+        resourceManager.credibility = 50;
+
+        resourceManager.youthSupport = 50;
+        resourceManager.workingClassSupport = 50;
+        resourceManager.corporateSupport = 50;
         
+        resourceManager.adsCost = 100;
+        resourceManager.rallyCost = 150;
+        resourceManager.prCost = 200;
+        
+        resourceManager.popularityDollarValue = 25;
+        resourceManager.credibilityDollarValue = 20;
+        
+        resourceManager.fundraisingBonus = 0f;
+        resourceManager.prBonus = 0f;
+        resourceManager.rallyBonus = 0f;
+        resourceManager.adsPenalty = 0f;
+        
+        resourceManager.selectedRoute = "None";
+        campaignMenuManager.routeChosen = false;
+        
+        resourceManager.adsBought = 0;
+        resourceManager.ralliesHeld = 0;
+        resourceManager.prCampaignsRun = 0;
+        
+        resourceManager.secretEndorsementUnlocked = false;
+        resourceManager.grassrootsMovementUnlocked = false;
+        resourceManager.corporateBackerUnlocked = false;
+        resourceManager.viralMomentUnlocked = false;
+        
+        campaignTimelineManager.currentDay = 1;
+        Save();
     }
 }
